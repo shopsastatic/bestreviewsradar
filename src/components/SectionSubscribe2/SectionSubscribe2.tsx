87f40a71fcc30aaf0db84b41ff4e58356400f482
Khai Dang <akhai7230@gmail.com>
2024-10-23 08:50:54 +0700

    Update

--- src/components/SectionSubscribe2/SectionSubscribe2.tsx
+++ src/components/SectionSubscribe2/SectionSubscribe2.tsx
@@ -14,45 +14,14 @@ const SectionSubscribe2: FC<SectionSubscribe2Props> = ({ className = '' }) => {
 	}
 
 	return (
-		<div
-			className={`nc-SectionSubscribe2 relative flex flex-col items-center lg:flex-row ${className}`}
-		>
-			<div className="mb-14 flex-shrink-0 lg:mb-0 lg:me-10 lg:w-2/5">
-				<h2 className="text-4xl font-semibold">
-					{NC_SITE_SETTINGS.newsletter_section?.title}
-				</h2>
-				<span className="mt-6 block text-neutral-500 dark:text-neutral-400">
-					{NC_SITE_SETTINGS.newsletter_section?.description}
-				</span>
-				<ul className="mt-10 space-y-5">
-					{NC_SITE_SETTINGS.newsletter_section?.features_list?.map(
-						(item, index) => (
-							<li
-								key={index}
-								className="flex items-center space-x-4 rtl:space-x-reverse"
-							>
-								<Badge
-									name={'0' + (index + 1)}
-									color={!index ? 'red' : index == 1 ? 'indigo' : 'green'}
-								/>
-								<span className="font-medium text-neutral-700 dark:text-neutral-300">
-									{item}
-								</span>
-							</li>
-						),
-					)}
-				</ul>
-				<AddSubscriberForm className="relative mt-10 max-w-sm" />
-			</div>
-			<div className="flex-grow">
-				<MyImage
-					alt="subsc"
-					sizes="(max-width: 768px) 100vw, 50vw"
-					src={NC_SITE_SETTINGS.newsletter_section?.right_image || ''}
-					width={1450}
-					height={638}
-				/>
-			</div>
+		<div className="flex-shrink-0 lg:w-2/5 m-auto">
+			<h2 className="text-4xl font-semibold text-center">
+				{NC_SITE_SETTINGS.newsletter_section?.title}
+			</h2>
+			<span className="mt-6 block text-neutral-500 dark:text-neutral-400">
+				{NC_SITE_SETTINGS.newsletter_section?.description}
+			</span>
+			<AddSubscriberForm className="relative mt-10 max-w-sm" />
 		</div>
 	)
 }
