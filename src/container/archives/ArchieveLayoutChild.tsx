ad1c7301fb5fa226478df53380dcb4e072fd65bb
Khai Dang <akhai7230@gmail.com>
2024-10-31 15:05:16 +0700

    Update

--- src/container/archives/ArchieveLayoutChild.tsx
+++ src/container/archives/ArchieveLayoutChild.tsx
@@ -43,14 +43,14 @@ const ArchiveLayoutChild: FC<any> = ({
 					<div className='category-breadcrumb ml-1 mt-2 flex items-center gap-2'>
 
 						<nav aria-label="Breadcrumb">
-							<ol className="breadcrumb hidden md:flex gap-2 items-center" itemScope itemType="https://schema.org/BreadcrumbList">
+							<ol className="breadcrumb hidden md:flex gap-1.5 items-center" itemScope itemType="https://schema.org/BreadcrumbList">
 								<li className="breadcrumb__item" itemProp="itemListElement" itemScope itemType="https://schema.org/ListItem">
 									<Link href="/" itemProp="item">
 										<span itemProp="name" className="hover:underline text-sm">Home</span>
 									</Link>
 									<meta itemProp="position" content="1" />
 								</li>
-								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" width="10" height="10"><path d="M96 480c-8.188 0-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L242.8 256L73.38 86.63c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l192 192c12.5 12.5 12.5 32.75 0 45.25l-192 192C112.4 476.9 104.2 480 96 480z" fill="#5D6266" /></svg>
+								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" width="9" height="9" className='mt-0.5'><path d="M96 480c-8.188 0-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L242.8 256L73.38 86.63c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l192 192c12.5 12.5 12.5 32.75 0 45.25l-192 192C112.4 476.9 104.2 480 96 480z" fill="#5D6266" /></svg>
 
 								<li className="breadcrumb__item flex items-center" itemProp="itemListElement" itemScope itemType="https://schema.org/ListItem">
 									<Link href={parent?.node?.uri ?? '/'} itemProp="item">
@@ -58,7 +58,7 @@ const ArchiveLayoutChild: FC<any> = ({
 									</Link>
 									<meta itemProp="position" content="2" />
 								</li>
-								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" width="10" height="10"><path d="M96 480c-8.188 0-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L242.8 256L73.38 86.63c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l192 192c12.5 12.5 12.5 32.75 0 45.25l-192 192C112.4 476.9 104.2 480 96 480z" fill="#5D6266" /></svg>
+								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" width="9" height="9" className='mt-0.5'><path d="M96 480c-8.188 0-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L242.8 256L73.38 86.63c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l192 192c12.5 12.5 12.5 32.75 0 45.25l-192 192C112.4 476.9 104.2 480 96 480z" fill="#5D6266" /></svg>
 
 								<li className="breadcrumb__item" itemProp="itemListElement" itemScope itemType="https://schema.org/ListItem" aria-current="page">
 									<span itemProp="name" className='text-sm'>{name}</span>
--- src/container/singles/SingleHeader.tsx
+++ src/container/singles/SingleHeader.tsx
@@ -106,14 +106,14 @@ const SingleHeader: FC<SingleHeaderProps> = ({
         {!nocategory && (
           <>
             <nav aria-label="Breadcrumb">
-              <ol className="breadcrumb hidden md:flex gap-2 items-center" itemScope itemType="https://schema.org/BreadcrumbList">
+              <ol className="breadcrumb hidden md:flex gap-1.5 items-center" itemScope itemType="https://schema.org/BreadcrumbList">
                 <li className="breadcrumb__item" itemProp="itemListElement" itemScope itemType="https://schema.org/ListItem">
                   <Link href="/" itemProp="item">
                     <span itemProp="name" className="text-[13px] text-[#5B5E61]">Home</span>
                   </Link>
                   <meta itemProp="position" content="1" />
                 </li>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" width="10" height="10"><path d="M96 480c-8.188 0-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L242.8 256L73.38 86.63c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l192 192c12.5 12.5 12.5 32.75 0 45.25l-192 192C112.4 476.9 104.2 480 96 480z" fill="#5D6266" /></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" width="9" height="9" className="mt-1"><path d="M96 480c-8.188 0-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L242.8 256L73.38 86.63c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l192 192c12.5 12.5 12.5 32.75 0 45.25l-192 192C112.4 476.9 104.2 480 96 480z" fill="#5D6266" /></svg>
 
                 {cate?.slice(0, -1)?.map((item: any, index: any) => (
                   <React.Fragment key={item.id || index}>
@@ -123,7 +123,7 @@ const SingleHeader: FC<SingleHeaderProps> = ({
                       </Link>
                       <meta itemProp="position" content="2" />
                     </li>
-                    {cate && index < cate.length - 1 && <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" width="10" height="10"><path d="M96 480c-8.188 0-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L242.8 256L73.38 86.63c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l192 192c12.5 12.5 12.5 32.75 0 45.25l-192 192C112.4 476.9 104.2 480 96 480z" fill="#5D6266" /></svg>}
+                    {cate && index < cate.length - 1 && <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" className="mt-1" width="9" height="9"><path d="M96 480c-8.188 0-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L242.8 256L73.38 86.63c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l192 192c12.5 12.5 12.5 32.75 0 45.25l-192 192C112.4 476.9 104.2 480 96 480z" fill="#5D6266" /></svg>}
                   </React.Fragment>
                 ))}
 
