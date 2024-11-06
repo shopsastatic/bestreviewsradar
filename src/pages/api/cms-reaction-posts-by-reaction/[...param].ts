27c94433bd812e7fe68ad91482a05be797e35a9a
Khai Dang <akhai7230@gmail.com>
2024-11-06 15:23:35 +0700

    Update

--- src/pages/api/cms-reaction-posts-by-reaction/[...param].ts
+++ src/pages/api/cms-reaction-posts-by-reaction/[...param].ts
@@ -1,11 +1,9 @@
-import { QueryGetAllReactionPostsByUserAndReactionQuery } from "@/__generated__/graphql";
 import { GET_USER_REACTION_POSTS_FIRST_COMMON } from "@/contains/contants";
-import { GET_ALL_REACTION_POSTS_BY_USER_AND_REACTION } from "@/fragments/queries";
 import { getApolloClient } from "@faustwp/core";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export type CMSReactionPostsResponseData = {
-  data?: QueryGetAllReactionPostsByUserAndReactionQuery | null;
+  data?: any | null;
   error?: string | null;
 };
 
@@ -22,25 +20,5 @@ export default async function handler(
   if (!userID || !reaction) {
     res.status(400).send({ error: "Param is required and must like: /1/save" });
     return;
-  }
-
-  try {
-    const { data, error, errors } = await client
-      .query({
-        query: GET_ALL_REACTION_POSTS_BY_USER_AND_REACTION,
-        variables: {
-          search: reaction,
-          first: GET_USER_REACTION_POSTS_FIRST_COMMON,
-          id: userID,
-        },
-      })
-      .then((result) => result);
-    if (error || errors) {
-      res.status(500).send({ error: "Failed to fetch data" });
-      return;
-    }
-    res.status(200).send({ data });
-  } catch (err) {
-    res.status(500).send({ error: "Failed to fetch data" });
   }
 }
