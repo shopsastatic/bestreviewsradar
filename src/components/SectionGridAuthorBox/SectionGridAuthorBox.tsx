2661e44a11f4cfa11eeeb27ba115cf473c3628f8
Khai Dang <akhai7230@gmail.com>
2024-11-06 15:10:46 +0700

    Update

--- src/components/SectionGridAuthorBox/SectionGridAuthorBox.tsx
+++ src/components/SectionGridAuthorBox/SectionGridAuthorBox.tsx
@@ -1,11 +1,10 @@
 import { FragmentType } from '@/__generated__'
-import CardAuthorBox from '@/components/CardAuthorBox/CardAuthorBox'
 import { NC_USER_FULL_FIELDS_FRAGMENT } from '@/fragments'
 import { FC } from 'react'
 
 export interface SectionGridAuthorBoxProps {
 	className?: string
-	authors: FragmentType<typeof NC_USER_FULL_FIELDS_FRAGMENT>[]
+	authors: FragmentType<any>[]
 }
 
 const SectionGridAuthorBox: FC<SectionGridAuthorBoxProps> = ({
@@ -15,13 +14,7 @@ const SectionGridAuthorBox: FC<SectionGridAuthorBoxProps> = ({
 	return (
 		<div className={`nc-SectionGridAuthorBox relative ${className}`}>
 			<div className="grid grid-cols-2 gap-4 sm:grid-cols-3 md:gap-8 lg:grid-cols-4 xl:grid-cols-5">
-				{authors.map(author => (
-					<CardAuthorBox
-						// @ts-ignore
-						key={author.databaseId}
-						author={author}
-					/>
-				))}
+				
 			</div>
 		</div>
 	)
