bf0248dc63ae3c852237522f5654431a1e3ae15d
Khai Dang <akhai7230@gmail.com>
2024-11-13 10:17:49 +0700

    Update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import "../../faust.config";
 import React from "react";
+import { Inter, Noto_Sans } from "next/font/google";
 import { useRouter } from "next/router";
 import { FaustProvider } from "@faustwp/core";
 import "@/styles/globals.css";
@@ -7,19 +8,25 @@ import "@/styles/index.scss";
 import { AppProps } from "next/app";
 import { WordPressBlocksProvider, fromThemeJson } from "@faustwp/blocks";
 import blocks from "@/wp-blocks";
-import { Inter, Noto_Sans} from "next/font/google";
 import SiteWrapperProvider from "@/container/SiteWrapperProvider";
 import { Toaster } from "react-hot-toast";
 import NextNProgress from "nextjs-progressbar";
 import themeJson from "../../theme.json";
 import { GoogleAnalytics } from "nextjs-google-analytics";
 
-const inter = Inter({
+export const inter = Inter({
   weight: ["400", "600", "700", "800"],
   preload: true,
   subsets: ["latin"],
   variable: "--inter-font",
-})
+});
+
+export const noto_san = Noto_Sans({
+  weight: ["400", "600", "700", "800"],
+  preload: true,
+  subsets: ["latin"],
+  variable: "--noto-font",
+});
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
@@ -27,7 +34,6 @@ export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <GoogleAnalytics trackPageViews />
-
       <FaustProvider pageProps={pageProps}>
         <WordPressBlocksProvider
           config={{
@@ -36,18 +42,30 @@ export default function MyApp({ Component, pageProps }: AppProps) {
           }}
         >
           <SiteWrapperProvider {...pageProps}>
-            <NextNProgress color="#818cf8" />
-            <Component {...pageProps} key={router.asPath} />
-            <Toaster
-              position="bottom-left"
-              toastOptions={{
-                style: {
-                  fontSize: "14px",
-                  borderRadius: "0.75rem",
-                },
-              }}
-              containerClassName="text-sm"
-            />
+          <style jsx global>{`
+              html, body, .large-width, .prod-child {
+                font-family: ${inter.style.fontFamily};
+              }
+              .heading-tag-text {
+                font-family: ${noto_san.style.fontFamily};
+              }
+            `}</style>
+
+            {/* Apply the font classes to the main wrapper */}
+            <main>
+              <NextNProgress color="#818cf8" />
+              <Component {...pageProps} key={router.asPath} />
+              <Toaster
+                position="bottom-left"
+                toastOptions={{
+                  style: {
+                    fontSize: "14px",
+                    borderRadius: "0.75rem",
+                  },
+                }}
+                containerClassName="text-sm"
+              />
+            </main>
           </SiteWrapperProvider>
         </WordPressBlocksProvider>
       </FaustProvider>
