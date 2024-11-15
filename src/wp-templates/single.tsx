d04c5b957789fa2bf039c0a101ee79b7d1332424
Khai Dang <akhai7230@gmail.com>
2024-11-15 14:35:59 +0700

    Update

--- src/wp-templates/single.tsx
+++ src/wp-templates/single.tsx
@@ -10,6 +10,7 @@ import { FOOTER_LOCATION, PRIMARY_LOCATION } from "@/contains/menu";
 import { useRouter } from "next/router";
 import SingleHeader from "@/container/singles/SingleHeader";
 import { useEffect, useState } from "react";
+import SingleContent from "@/container/singles/SingleContent";
 
 const Component: FaustTemplate<GetPostSiglePageQuery> = (props: any) => {
   const router = useRouter();
@@ -42,7 +43,8 @@ const Component: FaustTemplate<GetPostSiglePageQuery> = (props: any) => {
           props.data?.generalSettings as NcgeneralSettingsFieldsFragmentFragment
         }
       >
-        1
+        <SingleHeader post={_post} />
+        <SingleContent post={_post} />
       </PageLayout>
     </>
   );
