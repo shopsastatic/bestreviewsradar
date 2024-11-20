19df69b58c41612cfcea2f14ebea55c31a51b9ed
Khai Dang <akhai7230@gmail.com>
2024-11-20 22:43:10 +0700

    Update

--- src/pages/api/revalidate.js
+++ src/pages/api/revalidate.js
@@ -6,11 +6,15 @@ export default async function handler(req, res) {
   try {
       const paths = req.query.paths ? req.query.paths.split(',') : ['/'];
 
+      // Revalidate các URL frontend
       for (const path of paths) {
-          // Thêm tham số noCache vào đường dẫn
-          const pathWithNoCache = `${path}?noCache=1`;
-          await res.revalidate(pathWithNoCache);
+          await res.revalidate(path);
       }
+
+      // Gửi một HEAD request đến GraphQL API để bypass cache
+      await fetch(`${process.env.NEXT_PUBLIC_WORDPRESS_URL}/graphql?noCache=1`, {
+          method: 'HEAD', // Chỉ gửi request để làm mới cache, không fetch data
+      });
 
       return res.json({ revalidated: true });
   } catch (err) {
