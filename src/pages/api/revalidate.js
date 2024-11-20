0221067cdf357d6ebf0e671546b549ec63cf35e5
Khai Dang <akhai7230@gmail.com>
2024-11-20 22:25:41 +0700

    Update

--- src/pages/api/revalidate.js
+++ src/pages/api/revalidate.js
@@ -1,19 +1,19 @@
-// pages/api/revalidate.js
 export default async function handler(req, res) {
-    if (req.query.secret !== process.env.MY_SECRET_TOKEN) {
+  if (req.query.secret !== process.env.MY_SECRET_TOKEN) {
       return res.status(401).json({ message: 'Invalid token' });
-    }
-  
-    try {
-      const paths = req.query.paths ? req.query.paths.split(',') : ['/'];
-  
-      for (const path of paths) {
-        await res.revalidate(path);
-      }
-  
-      return res.json({ revalidated: true });
-    } catch (err) {
-      return res.status(500).json({ message: 'Error revalidating', error: err });
-    }
   }
-  
+
+  try {
+      const paths = req.query.paths ? req.query.paths.split(',') : ['/'];
+
+      for (const path of paths) {
+          // Thêm tham số noCache vào đường dẫn
+          const pathWithNoCache = `${path}?noCache=1`;
+          await res.revalidate(pathWithNoCache);
+      }
+
+      return res.json({ revalidated: true });
+  } catch (err) {
+      return res.status(500).json({ message: 'Error revalidating', error: err });
+  }
+}
