df2b9e612872db2751541a0bc269d3b8033ad784
Khai Dang <akhai7230@gmail.com>
2024-11-21 09:25:32 +0700

    Update

--- src/pages/api/revalidate.js
+++ src/pages/api/revalidate.js
@@ -1,23 +1,19 @@
+// pages/api/revalidate.js
 export default async function handler(req, res) {
-  if (req.query.secret !== process.env.MY_SECRET_TOKEN) {
+    if (req.query.secret !== process.env.MY_SECRET_TOKEN) {
       return res.status(401).json({ message: 'Invalid token' });
-  }
-
-  try {
+    }
+  
+    try {
       const paths = req.query.paths ? req.query.paths.split(',') : ['/'];
-
-      // Revalidate các URL frontend
+  
       for (const path of paths) {
-          await res.revalidate(path);
+        await res.revalidate(path);
       }
-
-      // Gửi một HEAD request đến GraphQL API để bypass cache
-      await fetch(`${process.env.NEXT_PUBLIC_WORDPRESS_URL}/graphql?noCache=1`, {
-          method: 'HEAD', // Chỉ gửi request để làm mới cache, không fetch data
-      });
-
+  
       return res.json({ revalidated: true });
-  } catch (err) {
+    } catch (err) {
       return res.status(500).json({ message: 'Error revalidating', error: err });
+    }
   }
-}
+  
