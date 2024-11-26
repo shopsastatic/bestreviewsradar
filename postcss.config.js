39aee2870e5c5d56d010f4486eca4c2703a2a9c8
Khai Dang <akhai7230@gmail.com>
2024-11-26 16:41:39 +0700

    Update

--- postcss.config.js
+++ postcss.config.js
@@ -1,20 +1,6 @@
-const purgecss = require('@fullhuman/postcss-purgecss');
-
-const isProduction = process.env.NODE_ENV === 'production';
-
 module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
-    ...(isProduction && {
-      purgecss: purgecss({
-        content: [
-          './pages/**/*.{js,jsx,ts,tsx}',
-          './components/**/*.{js,jsx,ts,tsx}',
-        ],
-        safelist: ['safelisted-class'], // Add any classes you want to keep
-        defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-      }),
-    }),
   },
 };
