e6f527ac8af2c13ad2d106f948ee0cae68390ab9
Khai Dang <akhai7230@gmail.com>
2024-11-29 09:48:41 +0700

    Update

--- src/wp-templates/single.tsx
+++ src/wp-templates/single.tsx
@@ -47,8 +47,8 @@ const Component: FaustTemplate<GetPostSiglePageQuery> = (props: any) => {
           props.data?.generalSettings as NcgeneralSettingsFieldsFragmentFragment
         }
       >
-        <SingleHeader post={_post} />
-        {/* <SingleContent post={_post} /> */}
+        {/* <SingleHeader post={_post} /> */}
+        <SingleContent post={_post} />
       </PageLayout>
     </>
   );
