c22cca77ca8a25f8facccad10c4e19e2686a00e5
Khai Dang <akhai7230@gmail.com>
2024-12-02 09:54:18 +0700

    Update

--- next.config.js
+++ next.config.js
@@ -12,6 +12,11 @@ module.exports = withFaust({
     optimizeCss: true,
     granularChunks: true,
   },
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production' ? {
+      exclude: ['error', 'warn'],
+    } : false,
+  },
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     config.optimization.splitChunks = {
       chunks: 'all',
