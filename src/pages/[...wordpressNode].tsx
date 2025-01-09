93d853e9c857a6aa134771128bc86b86c7cdb5c3
Khai Dang <akhai7230@gmail.com>
2025-01-09 22:35:22 +0700

    Update

--- src/pages/[...wordpressNode].tsx
+++ src/pages/[...wordpressNode].tsx
@@ -9,22 +9,22 @@ export default function Page(props: WordPressTemplateProps) {
 export async function myGetPaths() {
   const response = await fetch(
     process.env.NEXT_PUBLIC_WORDPRESS_URL?.replace(/\/$/, "") +
-      "/wp-json/wp/v2/posts?per_page=500&_fields=slug"
+      "/wp-json/wp/v2/posts?per_page=20&_fields=slug"
   );
   const getAllCategories = await fetch(
     process.env.NEXT_PUBLIC_WORDPRESS_URL?.replace(/\/$/, "") +
-      "/wp-json/wp/v2/categories?per_page=500&_fields=slug"
+      "/wp-json/wp/v2/categories?per_page=20&_fields=slug"
   );
 
   let posts = (await response.json()) as any[];
   let categories = (await getAllCategories.json()) as any[];
 
-  if (!categories?.length) {
-    categories = [{ slug: "uncategorized" }];
-  }
-  if (!posts?.length) {
-    posts = [{ slug: "hello-world" }];
-  }
+  // if (!categories?.length) {
+  //   categories = [{ slug: "uncategorized" }];
+  // }
+  // if (!posts?.length) {
+  //   posts = [{ slug: "hello-world" }];
+  // }
 
   posts = [
     ...categories.map((category) => ({ slug: "/" + category.slug })),
